88643064d1e858623f7db0215b9c7c84a44021ca
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2022-12-17 13:07:19 -0500

    changed the way sims worked. added inheritance

--- sim.py
+++ sim.py
@@ -1,5 +1,6 @@
 import random
 import statistics
+import matplotlib.pyplot as plt
 
 print("WE OUT")
 
@@ -10,123 +11,97 @@ def getProportionAccepted(l:list) -> float:
     return (sum(1.0 for element in l if element) / len(l))
 
 def getProportionDiff(list1:list, list2:list) -> float:
+    """RETURNS THE DIFFERENCE IN PROPORTIONS. LIST2-LIST1"""
     return getProportionAccepted(list2) - getProportionAccepted(list1)
 
-class GIO():
+
+
+class PERSON():
+    def __init__(self, crusty_n_girls, crusty_n_boys, nice_n_girls, nice_n_boys) -> None:
+        self.CRUSTY_N_GIRLS = crusty_n_girls
+        self.CRUSTY_N_BOYS = crusty_n_boys
+        self.NICE_N_GIRLS = nice_n_girls
+        self.NICE_N_BOYS = nice_n_boys
+        self.generate_random()
+
+    def generate_random(self):
+        self.crustygirls = [pickedCandy() for a in range(self.CRUSTY_N_GIRLS)]
+        self.crustyboys = [pickedCandy() for a in range(self.CRUSTY_N_BOYS)]
+        self.nicegirls = [pickedCandy() for a in range(self.NICE_N_GIRLS)]
+        self.niceboys = [pickedCandy() for a in range(self.NICE_N_BOYS)]
+
+    def simulate_gender_diff(self, trials:int) ->float:
+        results = []
+        for i in range(trials):
+            self.generate_random()
+            results.append(getProportionDiff(self.crustyboys, self.niceboys) - getProportionDiff(self.crustygirls, self.nicegirls))
+        return results
+
+
+    def simulate_clothing(self, trials:int):
+        results = []
+        for i in range(trials):
+            self.generate_random()
+            results.append(getProportionDiff(self.crustyboys + self.crustygirls, self.niceboys + self.nicegirls))
+        return results
+
+    def analyze_trials(self, y, trials:list):
+        percent_greater_than_observed = (sum([1.0 for x in trials if x >= y]) / len(trials)) * 100
+        if percent_greater_than_observed <= 5:
+            print('Statistically significant. Only ' + str(percent_greater_than_observed) + '% of trials achieved a difference greater than ' + str(y))
+        else:
+            print('falls within natural error. ' + str(percent_greater_than_observed) + '% of trials achieved a difference greater than ' + str(y))
+        plt.hist(trials, 100)
+        plt.axvline(y, color='k', linestyle='dashed', linewidth=2)
+        plt.show()
+
+
+
+    
+class GIO(PERSON):
     CRUSTY_N_GIRLS = 10
     CRUSTY_N_BOYS = 10
     NICE_N_GIRLS = 10
     NICE_N_BOYS = 10
 
     def __init__(self) -> None:
-        self.crustygirls = [pickedCandy() for a in range(GIO.CRUSTY_N_GIRLS)]
-        self.crustyboys = [pickedCandy() for a in range(GIO.CRUSTY_N_BOYS)]
-        self.nicegirls = [pickedCandy() for a in range(GIO.NICE_N_GIRLS)]
-        self.niceboys = [pickedCandy() for a in range(GIO.NICE_N_BOYS)]
+        PERSON.__init__(self,
+            GIO.CRUSTY_N_GIRLS,
+            GIO.CRUSTY_N_BOYS,
+            GIO.NICE_N_GIRLS,
+            GIO.NICE_N_BOYS
+        )
 
-    def compare_proportion_differences(self) ->float:
-        return getProportionDiff(self.crustyboys, self.niceboys) - getProportionDiff(self.crustygirls, self.nicegirls)
-   
-
-class GENE():
+class GENE(PERSON):
     CRUSTY_N_GIRLS = 10
     CRUSTY_N_BOYS = 10
     NICE_N_GIRLS = 10
     NICE_N_BOYS = 10
 
     def __init__(self) -> None:
-        self.crustygirls = [pickedCandy() for a in range(GIO.CRUSTY_N_GIRLS)]
-        self.crustyboys = [pickedCandy() for a in range(GIO.CRUSTY_N_BOYS)]
-        self.nicegirls = [pickedCandy() for a in range(GIO.NICE_N_GIRLS)]
-        self.niceboys = [pickedCandy() for a in range(GIO.NICE_N_BOYS)]
+        PERSON.__init__(self,
+            GENE.CRUSTY_N_GIRLS,
+            GENE.CRUSTY_N_BOYS,
+            GENE.NICE_N_GIRLS,
+            GENE.NICE_N_BOYS
+        )
 
-    def compare_proportion_differences(self) ->float:
-        return getProportionDiff(self.crustyboys, self.niceboys) - getProportionDiff(self.crustygirls, self.nicegirls)
-
-
-class TEDDY():
+class TEDDY(PERSON):
     CRUSTY_N_GIRLS = 10
     CRUSTY_N_BOYS = 10
     NICE_N_GIRLS = 10
     NICE_N_BOYS = 10
 
     def __init__(self) -> None:
-        self.crustygirls = [pickedCandy() for a in range(GIO.CRUSTY_N_GIRLS)]
-        self.crustyboys = [pickedCandy() for a in range(GIO.CRUSTY_N_BOYS)]
-        self.nicegirls = [pickedCandy() for a in range(GIO.NICE_N_GIRLS)]
-        self.niceboys = [pickedCandy() for a in range(GIO.NICE_N_BOYS)]
-
-    def compare_proportion_differences(self) ->float:
-        return getProportionDiff(self.crustyboys, self.niceboys) - getProportionDiff(self.crustygirls, self.nicegirls)
-
-HOW_MANY_RUNS = 50000
-def initial_sim():
-    results = []
-    for x in range(HOW_MANY_RUNS):
-
-        ##Take the difference in proportion accepted between crusty and nice for each persion, ignoring whether they are boys or girls. 
-        ##then, take the average difference
-        differences = []
-        gio = GIO()
-        differences.append(
-            getProportionAccepted(gio.crustyboys + gio.crustygirls) 
-            - getProportionAccepted(gio.niceboys + gio.nicegirls)
+        PERSON.__init__(self,
+            TEDDY.CRUSTY_N_GIRLS,
+            TEDDY.CRUSTY_N_BOYS,
+            TEDDY.NICE_N_GIRLS,
+            TEDDY.NICE_N_BOYS
         )
 
-        gene = GENE()
-        differences.append(
-            getProportionAccepted(gene.crustyboys + gene.crustygirls) 
-            - getProportionAccepted(gene.niceboys + gene.nicegirls)
-        )
-
-        teddy = TEDDY()
-        differences.append(
-            getProportionAccepted(teddy.crustyboys + teddy.crustygirls) 
-            - getProportionAccepted(teddy.niceboys + teddy.nicegirls)
-        )
-
-        results.append(statistics.mean(differences))
-
-    return results
-
-
-
-def sim_for_gender_diff():
-    results = []
-    for x in range(HOW_MANY_RUNS):
-        gio = GIO()
-        gio_diff = gio.compare_proportion_differences()
-
-        gene = GENE()
-        gene_diff = gene.compare_proportion_differences()
-
-        teddy = TEDDY()
-        teddy_diff = teddy.compare_proportion_differences()
-
-        results.append(statistics.mean([gio_diff, gene_diff, teddy_diff]))
-    return results
-
-
-def analyze_initial(our_average_difference):
-    trials = initial_sim()
-    percent_greater_than_observed = (sum([1.0 for x in trials if x >= our_average_difference]) / len(trials)) * 100
-    if percent_greater_than_observed <= 5:
-        print('Statistically significant. Only ' + str(percent_greater_than_observed) + '% of trials achieved a difference greater than ' + str(our_average_difference))
-    else:
-        print('falls within natural error. ' + str(percent_greater_than_observed) + '% of trials achieved a difference greater than ' + str(our_average_difference))
-
-def analyze_gender_diff(our_average_gender_proportion_difference):
-    trials = sim_for_gender_diff()
-    percent_greater_than_observed = (sum([1.0 for x in trials if x >= our_average_gender_proportion_difference]) / len(trials)) * 100
-    if percent_greater_than_observed <= 5:
-        print('Statistically significant. Only ' + str(percent_greater_than_observed) + '% of trials achieved a difference greater than ' + str(our_average_gender_proportion_difference))
-    else:
-        print('falls within natural error. ' + str(percent_greater_than_observed) + '% of trials achieved a difference greater than ' + str(our_average_gender_proportion_difference))
-
-analyze_gender_diff(.32)
-
-
-        
+gio = GIO()
+print(gio.simulate_clothing(100))
 
 
 
